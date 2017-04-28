a83da4e8594777007aa985804ce5dd987165a3c1
Michael Jackson <mike.jackson@bluequartz.net>
2017-04-27 21:34:45 -0400

    initial commit of all DREAM.3D source and common plugins

---
+++ scripts/sync_upstream.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ../
+cd DREAM3D
+git pull --rebase origin develop
+
+cd ExternalProjects/CMP
+git pull --rebase origin develop
+
+cd ../SIMPL
+git pull --rebase origin develop
+
+cd ../SIMPLView
+git pull --rebase origin develop
+
+cd ../Plugins/ITKImageProcessing
+git pull --rebase origin develop
+
+cd ../../../
+git status
