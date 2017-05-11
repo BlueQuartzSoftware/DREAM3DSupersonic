bba0e3718285c71e266b4c19345cd92582230d2a
Michael Jackson <mike.jackson@bluequartz.net>
2017-05-11 16:14:22 -0400

    Updating to latest revisions of DREAM3D and all the supporting repositories

--- scripts/sync_upstream.sh
+++ scripts/sync_upstream.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
+
+BRANCH="develop"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+function printHeader()
+{
+  echo -e "${GREEN}#------------------------------------------------------------------------"
+  echo -e "${RED} $1 ${NC}"
+}
+
+
 cd ../
 cd DREAM3D
+printHeader DREAM3D
 git pull --rebase origin develop
 
 cd ../CMP
+printHeader CMP
 git pull --rebase origin develop
 
 cd ../SIMPL
+printHeader SIMPL
 git pull --rebase origin develop
 
 cd ../SIMPLView
+printHeader SIMPLView
 git pull --rebase origin develop
 
-cd ../Plugins/ITKImageProcessing
-git pull --rebase origin develop
+cd ../Plugins
+PLUGINS=`ls`
+echo $PLUGINS
+for P in $PLUGINS; do
+    if [ -d "$P" ]; then
+        printHeader $P
+        cd $P
+        git pull --rebase origin develop
+        cd ../
+    fi
 
-cd ../../
-git status
+done
